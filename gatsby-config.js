585978880f23f6dd979cd07f1cf57268e26d27d9
Jakub Dreczkowski <kubadreczz@gmail.com>
2020-02-04 12:36:54 +0100

    Fix styles issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `Jakub Dreczkowski`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
